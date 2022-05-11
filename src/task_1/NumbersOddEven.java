ab7f3f5aaf88c053e4a1285ca409d733ddcc8f12
mdurap <cndurap@gmail.com>
2022-05-11 11:14:13 -0600

    Numbers Odd or Even

---
+++ src/task_1/NumbersOddEven.java
@@ -0,0 +1,12 @@
+package task_1;
+
+public class NumbersOddEven {
+    /*
+    Write a method which can identify given number is even or odd
+Example:
+identify(5)
+5)-->"
+identify(6)
+6)-->"
+     */
+}
