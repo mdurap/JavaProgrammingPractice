c8846a688f66bea328444b170810108f7b89f8e0
mdurap <cndurap@gmail.com>
2022-05-12 22:15:31 -0600

    frequency of characters

---
+++ src/task_2/FrequencyOfCharacters.java
@@ -0,0 +1,17 @@
+package task_2;
+
+public class FrequencyOfCharacters {
+    /*
+    Write a return method that can find the frequency of characters
+    Ex: FrequencyOfChars ("AAABBCDD") ==> A3B2C1D2
+     */
+
+    public static String frequencyMeth(String str){
+        for (int i = 0; i < str.length(); i++) {
+            int counter = 0;
+
+        }
+        return str;
+    }
+
+}
