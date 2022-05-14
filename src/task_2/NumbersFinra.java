e463d5dc62b0339f86afb4dfc6ab8a6eb556abd5
mdurap <cndurap@gmail.com>
2022-05-13 21:04:56 -0600

    Numbers FINRA example updated

--- src/task_2/NumbersFinra.java
+++ src/task_2/NumbersFinra.java
@@ -10,18 +10,17 @@ public class NumbersFinra {
      */
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num= input.nextInt();
-
-        if(num%15==0){
-            System.out.println("FINRA");
-        }else if(num%3==0){
-            System.out.println("FIN");
-        }else if(num%5==0){
-            System.out.println("RA");
-        }else{
-            System.out.println(num);
-        }
+//        Scanner input = new Scanner(System.in);
+//        int num= input.nextInt();
+//        if(num%15==0){
+//            System.out.println("FINRA");
+//        }else if(num%3==0){
+//            System.out.println("FIN");
+//        }else if(num%5==0){
+//            System.out.println("RA");
+//        }else{
+//            System.out.println(num);
+//        }
 
         System.out.println("==================");
 
