3b7d89af3dc3b4233cdfe5e41d9a047d73c93b4a
mdurap <cndurap@gmail.com>
2022-05-15 22:11:06 -0600

    divide two numbers without / operator

--- src/task_2/DivideWithoutOperator.java
+++ src/task_2/DivideWithoutOperator.java
@@ -1,10 +1,38 @@
 package task_2;
 
+import java.util.ArrayList;
+
 public class DivideWithoutOperator {
     /*
             Numbers Divide without / operator
         Write a method that can divide two numbers without using division operator (/)
      */
+    public static void main(String[] args) {
+        int number=35;
+        int num1=12;// 35/12 = result
+        int num2 = 0;
+        int num=0;
+        if(number>num1){
+            num=number-number%num1;
+        }
+
+
+        ArrayList <Integer> list= new ArrayList<>();
+        for (Integer i = 1; i <= num; i++) {
+            if(num%i==0){
+                list.add(i);
+            }
+        }
+        if(list.contains(num1)){
+             num2= list.size()-1-list.indexOf(num1);
+        }
+
+        System.out.println("Result is "+list.get(num2));
+
+//        System.out.println(list);
+//        System.out.println(list.size());
+
+    }
 
 
 
