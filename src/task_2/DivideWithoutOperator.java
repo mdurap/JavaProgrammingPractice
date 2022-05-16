4612f8bca2e710ea07f7fe2190e057ba69e29a84
mdurap <cndurap@gmail.com>
2022-05-15 21:27:24 -0600

    divide two numbers without / operator

---
+++ src/task_2/DivideWithoutOperator.java
@@ -0,0 +1,11 @@
+package task_2;
+
+public class DivideWithoutOperator {
+    /*
+            Numbers Divide without / operator
+        Write a method that can divide two numbers without using division operator (/)
+     */
+
+
+
+}
