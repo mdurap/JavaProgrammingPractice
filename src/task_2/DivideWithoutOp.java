f00c8a0b7c3cbe026ab501a0c89bacc00634f193
mdurap <cndurap@gmail.com>
2022-05-17 14:06:20 -0600

    divide two num updated

--- src/task_2/DivideWithoutOp.java
+++ src/task_2/DivideWithoutOp.java
@@ -1,23 +1,33 @@
 package task_2;
 
+import java.util.Scanner;
+
 public class DivideWithoutOp {
     /*
              Numbers Divide without / operator
          Write a method that can divide two numbers without using division operator (/)
       */
     public static void main(String[] args) {
-        int num1 = 36;
-        int num2 = 12;
+        System.out.println("Enter 2 numbers");
+        int num1 = new Scanner(System.in).nextInt();
+        int num2 = new Scanner(System.in).nextInt();
+        divideOperator(num1,num2);
+    }
+
+    private static int divideOperator(int num1, int num2) {
+
         int result=0;
 
-        if(num2==0){
-            System.out.println("invalid input");
 
-        }
+
         while (num1>=num2){
+            if(num2==0){
+                System.out.println("invalid input");
+                break;
+            }
             num1 = num1-num2;
             result++;
         }
-        System.out.println(result);
+        return result;
     }
 }
