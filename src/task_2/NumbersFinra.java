09f6bd0b8a88f0ec01c6994942e23d92d0c5be04
mdurap <cndurap@gmail.com>
2022-05-19 14:42:30 -0600

    finra updated

--- src/task_2/NumbersFinra.java
+++ src/task_2/NumbersFinra.java
@@ -32,7 +32,7 @@ public class NumbersFinra {
         String result="";
         String str="";
         for (int i = 1; i <=30; i++) {
-            if(i%15==0){
+            if(i%3==0 && i%5==0){
                result="FINRA";
             }else if(i%3==0){
                 result ="FIN";
