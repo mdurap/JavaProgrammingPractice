1f74787093f83f75a0db0ab9dadeac6c16f3f828
mdurap <cndurap@gmail.com>
2022-05-20 23:24:32 -0600

    FrequencyOfCharacters updated

--- src/task_2/FrequencyOfCharacters.java
+++ src/task_2/FrequencyOfCharacters.java
@@ -3,7 +3,7 @@ package task_2;
 public class FrequencyOfCharacters {
     /*
     Write a return method that can find the frequency of characters
-    Ex: FrequencyOfChars ("AAABBCDD") ==> A3B2C1D2
+    Ex: FrequencyOfChars ("AAABBCDDE") ==> A3B2C1D2E1
      */
 
     public static void main(String[] args) {
