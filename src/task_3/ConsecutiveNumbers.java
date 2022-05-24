b195b6749bea83df198713eb53bbb0b7be55ffb1
mdurap <cndurap@gmail.com>
2022-05-24 08:05:11 -0600

    consecutive numbers

--- src/task_3/ConsecutiveNumbers.java
+++ src/task_3/ConsecutiveNumbers.java
@@ -1,10 +1,12 @@
 package task_3;
 
+import java.util.Scanner;
+
 public class ConsecutiveNumbers {
     /*
     Write a function:
 that, given a positive integer N, prints the consecutive numbers
-from 1 to N, each on a separate line. However, any number divis ible by 2, 3 or 5
+from 1 to N, each on a separate line. However, any number divisible by 2, 3 or 5
 should be replaced by the word Codility , Test or Coders respectively. If a number is divisible by more than one of the numbers: 2,3 or 5, it should be
 replaced by a concatenation of the respective words Codility , Test and Coders in this given order. For example, numbers divisible by both 2 and 3
 should be replacée by CodilityTest and numbers divisible by all three numbers: 2,3 and 5, should be replaced by CodilityTestCoders
@@ -28,4 +30,30 @@ Codility
 17
 ....
      */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = input.nextInt();
+        for (int i = 0; i <= num; i++) {
+            if(i%5==0&&i%3==0&&i%2==0){
+                System.out.println("CodilityTestCoders");
+            }else if(i%5==0&&i%3==0){
+                System.out.println("TestCoders");
+            }else if(i%5==0&&i%2==0){
+                System.out.println("CodilityCoders");
+            }else if(i%3==0&&i%2==0){
+                System.out.println("CodilityTest");
+            }else if(i%2==0){
+                System.out.println("Codility");
+            }else if(i%3==0){
+                System.out.println("Test");
+            }else if(i%5==0){
+                System.out.println("Coders");
+            }else{
+                System.out.println(i);
+            }
+        }
+
+    }
+
 }
