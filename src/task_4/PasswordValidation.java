c1234ed8f3e6bb639ed8500c2ced6228f9ccf96a
mdurap <cndurap@gmail.com>
2022-05-31 14:44:05 -0600

    questions

---
+++ src/task_4/PasswordValidation.java
@@ -0,0 +1,14 @@
+package task_4;
+
+public class PasswordValidation {
+    /*
+    Q16: String - Password Validation Task
+Write a return method that can verify if a password is valid or not. Requirements:
+1. Password MUST be at least 6 characters and should not contain space
+2. Password should at least contain one upper case letter
+3. Password should at least contain one lowercase letter
+4. Password should at least contain one special characters
+5. Password should at least contain a digit
+6. if all requirements above are met, the method returns true, otherwise returns false
+     */
+}
---
+++ src/task_4/SumOfDigits.java
@@ -0,0 +1,8 @@
+package task_4;
+
+public class SumOfDigits {
+    /*
+    Q17: String - sum of digits in a string
+Write a method that can return the sum of the digits in a string
+     */
+}
