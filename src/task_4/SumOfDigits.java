584d56ee540b352085ac418e54e64f7b35ca8014
mdurap <cndurap@gmail.com>
2022-06-02 12:58:44 -0600

    Updated

--- src/task_4/SumOfDigits.java
+++ src/task_4/SumOfDigits.java
@@ -5,4 +5,16 @@ public class SumOfDigits {
     Q17: String - sum of digits in a string
 Write a method that can return the sum of the digits in a string
      */
+
+    public static void main(String[] args) {
+        String str = "2347";
+        int sum=0;
+        for (int i = 0; i < str.length(); i++) {
+            sum +=Integer.parseInt(""+str.charAt(i));
+        }
+        System.out.println(sum);
+    }
+
+
+
 }
