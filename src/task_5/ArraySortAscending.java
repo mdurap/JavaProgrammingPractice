93ea8b362290ad3f4d0252b3f77485aec7ddd98c
mdurap <cndurap@gmail.com>
2022-06-07 11:00:16 -0600

    Array Sort Ascending created

---
+++ src/task_5/ArraySortAscending.java
@@ -0,0 +1,9 @@
+package task_5;
+
+public class ArraySortAscending {
+    /*
+    Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
+    Ex: int [] arr = {10,9,8,7};
+    arr=sort(arr);==>{7,8,9,10};
+     */
+}
