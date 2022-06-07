7f362b26670abacd89b7600a8af55da621ccd7eb
mdurap <cndurap@gmail.com>
2022-06-07 12:00:33 -0600

    Array Sort Ascending updated

--- src/task_5/ArraySortAscending.java
+++ src/task_5/ArraySortAscending.java
@@ -10,7 +10,7 @@ public class ArraySortAscending {
      */
     public static void main(String[] args) {
         int [] arr = {10,9,8,7};
-        int temp;
+        int temp=0;
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = 0; j < arr.length-1; j++) {
                 if(arr[j]>arr[j+1]){
