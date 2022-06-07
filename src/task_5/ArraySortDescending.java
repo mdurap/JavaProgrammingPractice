b8499e58575518d5a08f50893f14b9209044b21e
mdurap <cndurap@gmail.com>
2022-06-07 12:00:48 -0600

    Array Sort Ascending updated

--- src/task_5/ArraySortDescending.java
+++ src/task_5/ArraySortDescending.java
@@ -10,7 +10,7 @@ public class ArraySortDescending {
      */
     public static void main(String[] args) {
         int [] arr = {10,20,7,8,90};
-        int temp;
+        int temp=0;
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = 0; j < arr.length-1; j++) {
                 if(arr[j]<arr[j+1]){
