ebabaa08fb909809222cda1ed9a5253356b703d9
mdurap <cndurap@gmail.com>
2022-06-08 10:41:05 -0600

    Array Sort Ascending updated

--- src/task_5/ArraySortAscending.java
+++ src/task_5/ArraySortAscending.java
@@ -20,7 +20,7 @@ public class ArraySortAscending {
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = 0; j < arr.length-1; j++) {
                 if(arr[j]>arr[j+1]){  // 10>9   {10,9,8,7};  {9,10,8,7};
-                    temp = arr[j+1];  // temp = 9-8
+                    temp = arr[j+1];  // temp = 9-8  swaping
                     arr[j+1] = arr[j];  //{10,10,8,7};    {9,10,10,7};
                     arr[j] = temp;     //{9,10,8,7};       {9,8,10,7};
                 }
