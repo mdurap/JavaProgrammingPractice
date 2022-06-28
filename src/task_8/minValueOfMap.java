e23d7605d0e4de154f478caca2f0bf9550caa814
mdurap <cndurap@gmail.com>
2022-06-28 08:44:37 -0600

    minimum value from a map

--- src/task_8/minValueOfMap.java
+++ src/task_8/minValueOfMap.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class minValueOfMap {
 
-    // write a method that can return the minimum value from a map(Don't use sort method)
+    // write a method that can return the minimum value from a map()
 
     public static void main(String[] args) {
 
