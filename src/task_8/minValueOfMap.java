4578b9c616396b4822009f13869059a6c2eabfb6
mdurap <cndurap@gmail.com>
2022-06-28 08:44:23 -0600

    minimum value from a map

---
+++ src/task_8/minValueOfMap.java
@@ -0,0 +1,33 @@
+package task_8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class minValueOfMap {
+
+    // write a method that can return the minimum value from a map(Don't use sort method)
+
+    public static void main(String[] args) {
+
+        Map<String,Integer> order = new HashMap<>();
+        order.put("tomato",6);
+        order.put("banana",2);
+        order.put("apple",1);
+        order.put("strawberry",7);
+        order.put("berry",-4);
+        order.put("cucumber",5);
+
+        System.out.println(order.values());
+        System.out.println(order.keySet());
+        int min = order.get("tomato");
+        for(Integer each : order.values()) {
+
+            if(each<min){
+                min=each;
+            }
+
+        }
+        System.out.println(min);
+    }
+
+}
